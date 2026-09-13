e3080198057352916fabc46aff898e846839f2ac
Harsh <drharshkumar499@gmail.com>
2026-09-13 23:23:35 +0530

    Leetcode_2011_Final_Value_of_Variable_After_OPerations

---
+++ 01_linearSearch/Leetcode_2011_Final_Value_of_Variable_After_OPerations.java
@@ -0,0 +1,18 @@
+public class Leetcode_2011_Final_Value_of_Variable_After_OPerations {
+    public static int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        for(int i=0;i<operations.length;i++){
+            if(operations[i].equals("X++") || operations[i].equals("++X")){
+                x++;
+            }else{
+                x--;
+            }
+        }
+        return x;
+    }
+    public static void main(String[] args) {
+        String[] operations = {"--X","X++","X++"};
+        int finalValue = finalValueAfterOperations(operations);
+        System.out.println("Final value of x: " + finalValue);
+    }
+}
